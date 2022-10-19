ba40ef84654954fc4ab198c8cec642752771db03
Blaga Nedyalkova <105053982+blaga-ned@users.noreply.github.com>
2022-10-19 23:54:47 +0100

    Add files via upload

---
+++ Java-Advanced/DefiningClasses/DefiningClassesLab/Car/Car.java
@@ -0,0 +1,45 @@
+package DefiningClasses.DefiningClassesLab.Car;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public Car (String brand, String model, int horsePower){
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+    public Car (String brand){
+        this(brand, "uknown", -1);
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setHorsePower(int horsePower){
+        this.horsePower = horsePower;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The car is: %s %s - %d HP.",
+                this.brand, this.model, this.horsePower);
+    }
+
+
+}
