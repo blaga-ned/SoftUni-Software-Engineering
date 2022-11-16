9637e8ad0e4a376fc78af94ff96d608811492e4d
Blaga Nedyalkova <105053982+blaga-ned@users.noreply.github.com>
2022-11-16 22:34:24 +0000

    Add files via upload

---
+++ Java-Advanced/DefiningClasses/DefiningClassesExercise/SpeedRacing/Car.java
@@ -0,0 +1,52 @@
+package DefiningClasses.DefiningClassesExercise.SpeedRacing;
+
+public class Car {
+    private String model;
+    private double fuelAmount;
+    private double fuelConsumptionPer1KM;
+    private int travelledDistance;
+
+    public Car(String model, double fuelAmount, double fuelConsumptionPer1KM) {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.fuelConsumptionPer1KM = fuelConsumptionPer1KM;
+        this.travelledDistance = 0;
+    }
+
+    public int getTravelledDistance() {
+        return travelledDistance;
+    }
+
+    public void setTravelledDistance(int travelledDistance) {
+        this.travelledDistance = travelledDistance;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public void setFuelAmount(double fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public double getFuelConsumptionPer1KM() {
+        return fuelConsumptionPer1KM;
+    }
+
+    public void setFuelConsumptionPer1KM(double fuelConsumptionPer1KM) {
+        this.fuelConsumptionPer1KM = fuelConsumptionPer1KM;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %d", model, fuelAmount, travelledDistance);
+    }
+}
