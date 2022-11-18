76b6c8bc593c074a28ebd14b3718c91c901c271b
Blaga Nedyalkova <105053982+blaga-ned@users.noreply.github.com>
2022-11-18 00:31:17 +0000

    Add files via upload

---
+++ JavaOOP/WorkingWithAbstraction/AbstractionLab/PointInRectangle_02.java
@@ -0,0 +1,9 @@
+package WorkingWithAbstraction.AbstractionLab;
+
+import java.util.Scanner;
+
+public class PointInRectangle_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
