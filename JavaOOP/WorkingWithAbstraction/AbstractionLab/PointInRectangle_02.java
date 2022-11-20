e6388df4ccde1545d09aa83803a3c15ef896e12f
Blaga Nedyalkova <105053982+blaga-ned@users.noreply.github.com>
2022-11-20 19:57:05 +0000

    Delete PointInRectangle_02.java

--- JavaOOP/WorkingWithAbstraction/AbstractionLab/PointInRectangle_02.java
+++
@@ -1,9 +0,0 @@
-package WorkingWithAbstraction.AbstractionLab;
-
-import java.util.Scanner;
-
-public class PointInRectangle_02 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
