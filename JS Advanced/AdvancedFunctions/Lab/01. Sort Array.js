e9ae732c0b166e0cb89cb8e44c14107df0223ed8
Blaga Nedyalkova <105053982+blaga-ned@users.noreply.github.com>
2023-07-20 10:16:35 +0100

    Adding little stories solutions

---
+++ JS Advanced/AdvancedFunctions/Lab/01. Sort Array.js
@@ -0,0 +1,9 @@
+function advancedFunctions(array, order) {
+    if (order === 'asc') {
+        array.sort((a, b) => a - b);
+    } else {
+        array.sort((a, b) => b - a);
+    }
+    return array;
+
+}
